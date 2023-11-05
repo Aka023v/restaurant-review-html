9176629fc932c886f3ff9e0334abfb82ea99d3c3
Aka023v <148590386+Aka023v@users.noreply.github.com>
2023-11-06 06:33:49 +0900

    作成

--- src/create.php
+++ src/create.php
@@ -1,18 +1,14 @@
 <body> 
-    <h1>新規作成</h1>
+    <h1>簡易Twitter</h1>
     <form method="post" action="./store.php">
         <table>
             <tr>
                 <td>投稿者名</td>
-                <td><input name="poster" placeholder="投稿者名" required></td>
+                <td><input name="poster" placeholder="投稿者名の記入欄" required></td>
             </tr>
             <tr>
-                <td>レストラン名</td>
-                <td><input name="restaurant_name" placeholder="レストラン名" required></td>
-            </tr>
-            <tr>
-                <td>レビュー</td>
-                <td><textarea name="review" placeholder="レビュー記入欄" required></textarea></td>
+                <td>投稿</td>
+                <td><textarea name="tweet" placeholder="ツイート記入欄" required></textarea></td>
             </tr>
             <tr>
                 <td ></td>
